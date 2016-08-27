588fbefa0eb0f1c3af956144a4bff98a66600679
Kevin Van de Mieroop <s079154@ap.be>
2016-08-27 23:41:01 +0200

    started configuring token authorisation, minor text fixes

--- public/js/controller.js
+++ public/js/controller.js
@@ -1,13 +1,15 @@
 angular.module("lightmeasurer",[]).controller("myController", function ($rootScope, $scope, $http, $interval)
     {
       LoadRepos();
+      LoadUser();
       function LoadRepos(){
           $http({
           method: 'GET',
           url: 'http://localhost:8080/repos'
           }).then (function successCallback(response) {
               $scope.repos = response;
-              console.log(response);
+              
+              //console.log(response);
           }, function errorCallback(response) {
           //alert("An error occured while fetching the user data!")
           });
@@ -18,10 +20,9 @@ angular.module("lightmeasurer",[]).controller("myController", function ($rootSco
             url: 'http://localhost:8080/user'
             }).then (function successCallback(response) {
 //                console.log("User loaded!");
-//                console.log(response);
-                $scope.currentuserid = 0;
+                console.log(response);
                 $scope.currentuser = response;
-                $scope.currentuserid = response.data.deviceid;
+                $scope.currentuserid = response.data.displayName;
 //                console.log($scope.currentuserid);
             }, function errorCallback(response) {
             //alert("An error occured while fetching the user data!")
