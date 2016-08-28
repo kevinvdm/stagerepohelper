44ef9c48d83b7b4e75f180738d2e4e3d740384c8
Kevin Van de Mieroop <s079154@ap.be>
2016-08-28 21:48:59 +0200

    days since last update added

--- public/js/controller.js
+++ public/js/controller.js
@@ -2,12 +2,22 @@ angular.module("lightmeasurer",[]).controller("myController", function ($rootSco
     {
       LoadRepos();
       LoadUser();
+      var currentDate = new Date();
+      $scope.daysSinceUpdate = [];
       function LoadRepos(){
           $http({
           method: 'GET',
           url: 'http://localhost:8080/repos'
           }).then (function successCallback(response) {
               $scope.repos = response;
+              for (i = 0; i < $scope.repos.data.length; i++)
+                {
+                  var date = Date.parse($scope.repos.data[i].updated_at)
+                  var dayssince = days_between(currentDate, date)
+                  console.log(dayssince);
+                  $scope.repos.data[i].dayssinceupdate = dayssince;
+                  //$scope.daysSinceUpdate.push(dayssince);
+                }
               //console.log(response);
           }, function errorCallback(response) {
           //alert("An error occured while fetching the user data!")
@@ -22,9 +32,25 @@ angular.module("lightmeasurer",[]).controller("myController", function ($rootSco
                 console.log(response);
                 $scope.currentuser = response;
                 $scope.currentuserid = response.data.displayName;
-//                console.log($scope.currentuserid);
+
             }, function errorCallback(response) {
             //alert("An error occured while fetching the user data!")
             });
         };
+        function days_between(date1, date2) {
+
+          // The number of milliseconds in one day
+          var ONE_DAY = 1000 * 60 * 60 * 24
+
+          // Convert both dates to milliseconds
+          var date1_ms = date1.getTime()
+          //var date2_ms = date2.getTime()
+
+          // Calculate the difference in milliseconds
+          var difference_ms = Math.abs(date1_ms - date2)
+
+          // Convert back to days and return
+          return Math.round(difference_ms/ONE_DAY)
+
+}
     });
